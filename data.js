a551229d95a843335b0223de0ee8ac55d332db34
Vishnu Saini <61437139+VishnuSaini07@users.noreply.github.com>
2021-08-23 10:57:57 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+  {
+    name: 'Tu Meri',
+    path: 'assets/musics/Bang Bang - Tu Meri.mp3',
+    artist: 'Vishal Dadlani',
+    cover: 'assets/images/Bang Bang.jpg'
+  },
+  {
+    name: 'Desi Kalakaar',
+    path: 'assets/musics/Desi Kalakaar.mp3',
+    artist: 'Yo Yo Honey Singh',
+    cover: 'assets/images/Desi Kalakaar.jpg'
+  },
+  {
+    name: 'Ki Samjhaiye',
+    path: 'assets/musics/Ki Samjhaiye.mp3',
+    artist: 'Amrinder Gill',
+    cover: 'assets/images/Amrinder Gill.jpg'
+  },
+  {
+    name: 'Mirzapur',
+    path: 'assets/musics/Mirzapur Theme Song.mp3',
+    artist: 'Anand Bhaskar',
+    cover: 'assets/images/mirzapur.jpg'
+  },
+  {
+    name: 'Dilli De Sardarboys',
+    path: 'assets/musics/Dilli de Sardarboys.mp3',
+    artist: 'Singhsta & Aparshakti Khurana',
+    cover: 'assets/images/dilli de sardarboys.jpg'
+  }
+]
